3059efe73ff61f30bbc9c262b035e8b5ba80e546
mwgnd <manuel.wgnd@mailbox.org>
2024-06-03 15:34:37 +0200

    fixed title error with private chats

--- inst/scrape_messages.py
+++ inst/scrape_messages.py
@@ -15,12 +15,15 @@ async def scrape_messages(client, channel_id):
           else:
               sender_id = message.peer_id.channel_id if message.is_channel else message.peer_id.user_id
               raw_text = "" if message.raw_text is None else message.raw_text.replace("\'", "\"")
+              title = "" if message.is_private is True else message.chat.title
+              views = "" if message.is_private is True else message.views
+              
               messages.append({
                   'channel_name': message.chat.username.lower(),
                   'channel_id': message.chat.id,
-                  'title': message.chat.title,
+                  'title': title,
                   'message_id': message.id,
-                  'message_views': message.views,
+                  'message_views': views,
                   'date': str(message.date),
                   'sender_id': sender_id,
                   'message_text': raw_text
@@ -38,6 +41,10 @@ async def scrape_messages(client, channel_id):
           else:
               sender_id = message.peer_id.channel_id if message.is_channel else message.peer_id.user_id
               raw_text = "" if message.raw_text is None else message.raw_text.replace("\'", "\"")
+              title = "" if message.is_private is True else message.chat.title
+              views = "" if message.is_private is True else message.views
+              
+              
               messages.append({
                   'channel_name': message.chat.username.lower(),
                   'channel_id': message.chat.id,
@@ -58,7 +65,7 @@ async def scrape_messages(client, channel_id):
 async def scrape_more_channels(client, channel_links):
     all_messages = []
     for channel_id in channel_links:
-        print(f"Scrape Channel: {channel_id}")
+        print(f"Scrape Messages from {channel_id}")
         messages = await scrape_messages(client, channel_id)
         all_messages.extend(messages)
         await asyncio.sleep(2)
